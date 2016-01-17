4bfec30c6a094cc77ae931fe59e094f9f5f19332
Andrew Nater <a.a.nater@gmail.com>
2016-01-17 13:47:35 -0500

    Attach city data to treemap nodes.

--- website/js/treemaps.js
+++ website/js/treemaps.js
@@ -1,15 +1,13 @@
 window.addEventListener("load", function(){
 
-  console.log("loaded");
-
   var tree = {
       name: "tree",
       children: [
-          { name: "JFK", size: 17 },
-          { name: "SFO", size: 15 },
-          { name: "LGA", size: 8 },
-          { name: "LGW", size: 5 },
-          { name: "DEN", size: 5 }
+          { name: "JFK", size: 17, city: "New York" },
+          { name: "SFO", size: 15, city: "San Francisco" },
+          { name: "LGA", size: 8, city: "New York" },
+          { name: "LGW", size: 5, city: "London" },
+          { name: "DEN", size: 5, city: "Denver" }
       ]
   };
 
@@ -21,7 +19,6 @@ window.addEventListener("load", function(){
   
   var treemap = d3.layout.treemap()
       .size([width, height])
-      .sticky(true)
       .value(function(d) { return d.size; });
   
   var node = div.datum(tree).selectAll(".node")
@@ -29,15 +26,16 @@ window.addEventListener("load", function(){
         .enter().append("div")
         .attr("class", "node")
         .call(position)
-        .style("background-color", function(d) {
-            return d.name == 'tree' ? '#fff' : color(d.parent.name); })
         .style("background-image", function (d) { 
           return "url(img/" + d.name + ".jpg)";})
         .append('div')
         .style("font-size", function(d) {
             // compute font size based on sqrt(area)
             return Math.max(20, 0.18*Math.sqrt(d.area))+'px'; })
-        .text(function(d) { return d.children ? null : d.name; });
+        .text(function(d) { return d.children ? null : d.name; })
+        .append('span')
+        .attr("class", "city")
+        .text(function(d){ return d.children ? null : d.city; });
 
   function position() {
     this.style("left", function(d) { return d.x + "px"; })
