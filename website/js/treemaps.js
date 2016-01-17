77f02384db593600b4a23f59898b6394f5a7a04a
Andrew Nater <a.a.nater@gmail.com>
2016-01-17 12:54:42 -0500

    Initial commit

---
+++ website/js/treemaps.js
@@ -0,0 +1,47 @@
+window.addEventListener("load", function(){
+
+  console.log("loaded");
+
+  var tree = {
+      name: "tree",
+      children: [
+          { name: "JFK", size: 30 },
+          { name: "SFO", size: 24 },
+          { name: "LGW", size: 9 },
+          { name: "LGA", size: 8 },
+          { name: "EWR", size: 7 }
+      ]
+  };
+
+  var width = 300,
+      height = 300,
+      color = d3.scale.category20c(),
+      div = d3.select("body").append("div")
+         .style("position", "relative");
+  
+  var treemap = d3.layout.treemap()
+      .size([width, height])
+      .sticky(true)
+      .value(function(d) { return d.size; });
+  
+  var node = div.datum(tree).selectAll(".node")
+        .data(treemap.nodes)
+        .enter().append("div")
+        .attr("class", "node")
+        .call(position)
+        .style("background-color", function(d) {
+            return d.name == 'tree' ? '#fff' : color(d.parent.name); })
+        .append('div')
+        .style("font-size", function(d) {
+            // compute font size based on sqrt(area)
+            return Math.max(20, 0.18*Math.sqrt(d.area))+'px'; })
+        .text(function(d) { return d.children ? null : d.name; });
+
+  function position() {
+    this.style("left", function(d) { return d.x + "px"; })
+        .style("top", function(d) { return d.y + "px"; })
+        .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+        .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+  }
+
+});
